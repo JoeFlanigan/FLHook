79c80d33f7e7496246a2d1cd1474fc888098648f
Joe Flanigan aka Rebirth <mert.torun97@outlook.de>
2016-01-03 11:48:12 +0100

    Headers are to be used...
    + Moved according lines to the header
    + Updated flPatch

--- trunk/acctpath_patch/src/Main.h
+++ trunk/acctpath_patch/src/Main.h
@@ -1,4 +1,22 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__ 1
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <math.h>
+#include <float.h>
+
+#define C __cdecl
+#define OFFSET_00 ('\xE9')
+#define OFFSET_10 ('\x81')
+#define OFFSET_11 ('\xEC')
+#define OFFSET_12 ('\x10')
+#define OFFSET_13 ('\x01')
+#define OFFSET_14 ('\x00')
+#define OFFSET_00_SZE (1)
+#define OFFSET_10_SZE (5)
+#define WIN32_LEAN_AND_MEAN
 
 #endif
